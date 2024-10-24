8b1a5ec04ea184a6883d0568d033b3012e929d9a
sangeethasr <sangeetharajan567@gmail.com>
2024-10-24 09:22:01 +0530

    Added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import Counter from "./components/Counter";
 import Form from "./components/Form";
 
 function App() {
   return (
-   <><Form/></>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Form />}></Route>
+          <Route path="/counter" element={<Counter/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
